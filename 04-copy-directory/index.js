565f382c40ac973d5444246abd2b65d3313352e3
treyer <nemereno_by@mail.ru>
2021-11-06 12:46:51 +0300

    feat: add copy directory functionality

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,37 @@
+const fs = require('fs');
+const path = require('path');
 
+const sourceFolder = path.join(__dirname, "files");
+const destinationFolder = path.join(__dirname, "files-copy");
+
+let copyDir = (sourceFolder, destinationFolder) => {
+    try{
+        //check if destination folder exists, create if not
+        fs.access(destinationFolder, err => {
+            if (err) {
+              fs.mkdir(destinationFolder, err => {
+                if(err) throw err;
+              })
+            }
+        })
+        //copy files
+        fs.promises.readdir(sourceFolder)
+        .then(filenames => {
+            for (let filename of filenames) {
+                let sourceFilePath = path.join(sourceFolder, filename);
+                let destFilePath = path.join(destinationFolder, filename);
+
+                fs.copyFile(sourceFilePath, destFilePath, (err) => {
+                    if (err) throw err;
+                });   
+            }
+        })
+        .catch(err => {
+            throw err;
+        })
+    } catch (err){
+        console.error(err);
+    }
+}
+
+copyDir (sourceFolder, destinationFolder);
